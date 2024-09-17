78f9bdc5901288b966551a5b053914c1e7b855d6
Fernando <fer.gab.sua@gmail.com>
2024-09-17 10:41:36 -0300

    edit models
    add models "Sucursales"
    edit model "DatosUser", add foreignkey "Sucursales"

--- mysite/pianoadherentes/models.py
+++ mysite/pianoadherentes/models.py
@@ -2,13 +2,20 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
+class Sucursales(models.Model):
+    id_sucursal = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.descripcion
+
 class DatosUser(models.Model):
     legajo = models.IntegerField(unique=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+    sucursal = models.ForeignKey(Sucursales, on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.user.username
-
 
 
 class Plan(models.Model):
